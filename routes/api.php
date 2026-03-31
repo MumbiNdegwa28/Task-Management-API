7abd3318071689bb0211877bb0940942f321e5ba
MumbiNdegwa <130558439+MumbiNdegwa@users.noreply.github.com>
2026-03-31 10:01:29 +0300

    debug: add env check route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,17 @@ use Illuminate\Support\Facades\Route;
  * IMPORTANT: /tasks/report must be registered BEFORE /tasks/{id}
  * otherwise Laravel matches "report" as an ID parameter.
  */
+
+Route::get('/debug-env', function () {
+    return response()->json([
+        'DB_CONNECTION' => env('DB_CONNECTION'),
+        'DB_HOST'       => env('DB_HOST'),
+        'DB_PORT'       => env('DB_PORT'),
+        'DB_DATABASE'   => env('DB_DATABASE'),
+        'DB_USERNAME'   => env('DB_USERNAME'),
+    ]);
+});
+
 Route::get('/tasks/report',         [TaskController::class, 'report']);
 Route::get('/tasks',                [TaskController::class, 'index']);
 Route::post('/tasks',               [TaskController::class, 'store']);
